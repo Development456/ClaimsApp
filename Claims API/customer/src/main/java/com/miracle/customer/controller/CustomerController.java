6f21322c8299d459de8f0e45cd92d2b28eb183af
ineeladri <110054194+ineeladri@users.noreply.github.com>
2023-01-05 20:25:03 +0530

    Update CustomerController.java

--- Claims API/customer/src/main/java/com/miracle/customer/controller/CustomerController.java
+++ Claims API/customer/src/main/java/com/miracle/customer/controller/CustomerController.java
@@ -27,7 +27,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="*")
 public class CustomerController {
 	/** The customer services. */
 	@Autowired
